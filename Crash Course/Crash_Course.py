d589f338afc5eb7c3303f752d62135a846207c3e
John LaTorre <johnjlatorre@gmail.com>
2019-08-08 19:31:59 -0400

    Daily practice 8/8/2019 Practice 7_a,b,1,2,3

--- Crash Course/Crash_Course.py
+++ Crash Course/Crash_Course.py
@@ -493,5 +493,45 @@ def practice6_11():
         print(city.title() + " is a city in " + where.title() + " with a general population of " + str(pop));
         print("It is a fact that " + city.title() + " is " + factoid + "\n");
 
-message = input("Tell me something, and I will repeat it back to you: ");
-print(message);
+def practice7_a():
+    height = input("How tall are you, in inches? ");
+    height = int(height);
+
+    if height >= 36:
+        print("\nYou're tall enough to ride!");
+    else:
+        print("\nYou'll be able to ride when you're a little older.");
+def practice7_b():
+    number = input("Enter a number, and I'll tell you if it's even or odd: ");
+    number = int(number);
+
+    if number % 2 == 0:
+        print("\nThe number " + str(number) + " is even.");
+    else:
+        print("\nThe number " + str(number) + " is odd.");
+
+def practice7_1():
+    car = input("What kind of car would you like to rent? ");
+    print("Let me see if I can find you a " + car.title());
+
+def practice7_2():
+    party = input("How many people are in your party? ");
+    party = int(party);
+
+    if party > 8:
+        print("I'm sorry you'll have to wait a bit for a table to be available.");
+    else:
+        print("Let me show you to your table.");
+
+def practice7_3():
+    number = input("Please provide a number: ");
+    number = int(number);
+
+    if number % 10 == 0:
+        print(str(number) + " is a multiple of ten.");
+    else:
+        print(str(number) + " is not a multiple of ten.");
+
+practice7_1();
+practice7_2();
+practice7_3();
